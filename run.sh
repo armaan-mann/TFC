ec481d58f87572a5782d38e6fb8c3827aca0cc52
armaan-mann <armaan.mann@mail.utoronto.ca>
2022-12-10 21:19:29 -0500

    done

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ cd Frontend-TFC
 npm start
 
 cd ../Backend-TFC
-python3 -m pip install virtualenv
 python3 -m virtualenv venv
+python3 -m pip install virtualenv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
--- startup.sh
+++ startup.sh
@@ -10,8 +10,9 @@ python3 -m virtualenv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+python3 manage.py migrate
 python3 manage.py makemigrations accounts
 python3 manage.py makemigrations studios
 python3 manage.py makemigrations classes
 python3 manage.py makemigrations subscriptions
-python3 manage.py migrate
+
